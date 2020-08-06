8ee55cd3dfe72852d46e88bee81d97ed9813a7c8
Ragnarok <Deathmetalldrummer@gmail.com>
2020-08-06 18:07:16 +0600

    Update portfolio.

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -5,9 +5,11 @@
         v-row(align="center" justify="center").height
           v-col(xs="12" sm="6")
             .portfolio__description
-              .headline {{item.name}}
+              h3.headline {{item.name}}
               br
-              .body-2(v-html="item.html")
+              p.body-2(v-html="item.desc")
+              ul
+                li(v-for="tech in item.tech") {{tech}}
               br
               v-btn(v-if='item.view' :href="item.view" target="_blank" outlined).mr-2 View
               v-btn(v-if='item.code' :href="item.code" target="_blank" outlined) Code
@@ -23,317 +25,175 @@
 			return {
 				portfolio: [
 					{
-						number: 1,
+						src: publicPath + './images/RNTO.jpg',
+						alt: 'RNTO',
+						name: 'RNTO',
+						code: null,
+						view: null,
+						desc: 'Админка для сайта визового центра.Проект с нуля, единственный фронтист на проекте.<br />Март - Июль 2020',
+						tech: ['Angular 9', 'NgRx', 'Material Angular', 'RESTful API']
+					},
+					{
+						src: publicPath + './images/TableUI.png',
+						alt: 'TableUI',
+						name: 'Table UI',
+						code: 'https://github.com/Deathmetalldrummer/TableUI',
+						view: 'https://deathmetalldrummer.github.io/TableUI/',
+						desc: 'Тестовое задание.<br />Май - Август 2020',
+						tech: ['Vue', 'Vuex']
+					},
+					{
+						src: publicPath + './images/GitHubSearch.png',
+						alt: 'GitHubSearch',
+						name: 'GitHubSearch',
+						code: 'https://github.com/Deathmetalldrummer/GitHubSearch',
+						view: 'https://deathmetalldrummer.github.io/GitHubSearch/',
+						desc: 'Тестовое задание.<br />Июль 2020.',
+						tech: ['Angular 9', 'NgRx', 'Material Angular', 'GitHub API']
+					},
+					{
 						src: publicPath + './images/Agency.jpg',
 						alt: 'Agency',
 						name: 'Agency',
 						code: 'https://github.com/Kirk-Terekhin/Landings/tree/gh-pages/Agency/',
 						view: 'https://kirk-terekhin.github.io/Landings/Agency/',
-            desc: 'Верстка лэндинга для практики.',
-            tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive'],
-            html: `
-                           <p>Верстка лэндинга для практики.</p>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                                <li>Bootstrap</li>
-                                <li>Adaptive</li>
-                                <li>Responsive</li>
-                            </ul>
-                            `
+						desc: 'Верстка лэндинга для практики.<br />2015',
+						tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive']
 					},
 					{
-						number: 2,
 						src: publicPath + './images/CorporateLandingPage.jpg',
 						alt: 'Corporate Landing Page',
 						name: 'Corporate Landing Page',
 						code: 'https://github.com/Kirk-Terekhin/Corporate-Landing-Page/',
 						view: 'https://kirk-terekhin.github.io/Corporate-Landing-Page/',
-            desc: 'Верстка лэндинга для практики.',
-            tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive'],
-            html: `
-                           <p>Верстка лэндинга для практики.</p>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                                <li>Bootstrap</li>
-                                <li>Adaptive</li>
-                                <li>Responsive</li>
-                            </ul>
-                            `
+						desc: 'Верстка лэндинга для практики.<br />2017',
+						tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive']
 					},
 					{
-            number: 3,
-            src: publicPath + './images/Logstyle.jpg',
+						src: publicPath + './images/Logstyle.jpg',
 						alt: 'Log-style',
 						name: 'Log-style',
 						code: 'https://github.com/Kirk-Terekhin/Landings/tree/gh-pages/logstyle/',
 						view: 'https://kirk-terekhin.github.io/Landings/logstyle',
-            desc: 'Копия сайта log-style.ru.',
-            tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive', 'WoW.js'],
-						html: `
-                           <p>Копия сайта log-style.ru.</p>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                                <li>Bootstrap</li>
-                                <li>Adaptive</li>
-                                <li>Responsive</li>
-                                <li>WoW.js</li>
-                            </ul>
-                            `
+						desc: 'Копия сайта log-style.ru.<br />2015',
+						tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive', 'WoW.js']
 					},
-          {
-            number: 4,
-            src: publicPath + './images/Gift.jpg',
+					{
+						src: publicPath + './images/Gift.jpg',
 						alt: 'Gift.kg',
 						name: 'Gift.kg',
 						code: 'https://github.com/Kirk-Terekhin/Gift/',
 						view: 'https://giftkg.000webhostapp.com/',
-            desc: 'Создание магазина новогодних подарков на CMS OpenCart.',
-            tech: ['HTML5', 'CSS3', 'OpenCart CMS'],
-						html: `
-                           <p>Создание магазина новогодних подарков на CMS OpenCart.</p>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                                <li>OpenCart CMS</li>
-                            </ul>
-                            `
+						desc: 'Создание магазина новогодних подарков на CMS OpenCart.<br />Ноябрь - Декабрь 2014',
+						tech: ['HTML5', 'CSS3', 'OpenCart CMS']
 					},
-          {
-            number: 5,
-            src: publicPath + './images/TZ.jpg',
+					{
+						src: publicPath + './images/TZ.jpg',
 						alt: 'TZ',
 						name: 'TZ',
 						code: 'https://github.com/Kirk-Terekhin/Landings/tree/gh-pages/TZ/',
 						view: 'https://kirk-terekhin.github.io/Landings/TZ/',
-            desc: 'Тестовое задание.',
-            tech: ['HTML5', 'CSS3', 'Adaptive', 'Responsive'],
-						html: `
-                           <p>Тестовое задание.</p>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                                <li>Adaptive</li>
-                                <li>Responsive</li>
-                            </ul>
-                            `
+						desc: 'Тестовое задание.<br />2015',
+						tech: ['HTML5', 'CSS3', 'Adaptive', 'Responsive']
 					},
-          {
-            number: 6,
-            src: publicPath + './images/LandingPage.jpg',
+					{
+						src: publicPath + './images/LandingPage.jpg',
 						alt: 'Landing Page',
 						name: 'Landing Page',
 						code: 'https://github.com/Kirk-Terekhin/Landings/tree/gh-pages/LandingPage/',
 						view: 'https://kirk-terekhin.github.io/Landings/LandingPage/',
-            desc: 'Верстка лэндинга для практики.',
-            tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive'],
-						html: `
-                           <p>Верстка лэндинга для практики.</p>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                                <li>Bootstrap</li>
-                                <li>Adaptive</li>
-                                <li>Responsive</li>
-                            </ul>
-                            `
+						desc: 'Верстка лэндинга для практики.<br />2016',
+						tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive']
 					},
-          {
-            number: 7,
-            src: publicPath + './images/NightVision.jpg',
+					{
+						src: publicPath + './images/NightVision.jpg',
 						alt: 'NightVision',
 						name: 'NightVision',
 						code: 'https://github.com/Kirk-Terekhin/Landings/tree/gh-pages/NightVision/',
 						view: 'https://kirk-terekhin.github.io/Landings/NightVision/',
-            desc: 'Верстка лэндинга для практики.',
-            tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive'],
-						html: `
-                           <p>Верстка лэндинга для практики.</p>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                                <li>Bootstrap</li>
-                                <li>Adaptive</li>
-                                <li>Responsive</li>
-                            </ul>
-                            `
+						desc: 'Верстка лэндинга для практики.<br />2015',
+						tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive']
 					},
-          {
-            number: 8,
-            src: publicPath + './images/Bootstrap.jpg',
+					{
+						src: publicPath + './images/Bootstrap.jpg',
 						alt: 'Bootstrap',
 						name: 'Bootstrap',
 						code: 'https://github.com/Kirk-Terekhin/Landings/tree/gh-pages/Bootstrap/',
 						view: 'https://kirk-terekhin.github.io/Landings/Bootstrap/',
-            desc: 'Верстка лэндинга для практики.Bootstrap.',
-            tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive'],
-						html: `
-                           <p>Верстка лэндинга для практики.Bootstrap.</p>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                                <li>Bootstrap</li>
-                                <li>Adaptive</li>
-                                <li>Responsive</li>
-                            </ul>
-                            `
+						desc: 'Верстка лэндинга для практики.Bootstrap.<br />2015',
+						tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive']
 					},
-          {
-            number: 9,
-            src: publicPath + './images/Sakura.jpg',
+					{
+						src: publicPath + './images/Sakura.jpg',
 						alt: 'Sakura',
 						name: 'Sakura',
 						code: 'https://github.com/Kirk-Terekhin/Sakura/',
 						view: 'https://kirk-terekhin.github.io/Sakura/',
-            desc: 'Верстка сайта.',
-            tech: ['HTML5', 'CSS3', 'jQuery', 'Adaptive', 'Responsive'],
-						html: `
-                           <p>Верстка сайта.</p>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                                <li>jQuery</li>
-                                <li>Adaptive</li>
-                                <li>Responsive</li>
-                            </ul>
-                            `
+						desc: 'Верстка сайта.<br />Декабрь 2015',
+						tech: ['HTML5', 'CSS3', 'jQuery', 'Adaptive', 'Responsive']
 					},
-          {
-            number: 10,
-            src: publicPath + './images/PageOnlineStore.jpg',
+					{
+						src: publicPath + './images/PageOnlineStore.jpg',
 						alt: 'PageOnlineStore',
 						name: 'PageOnlineStore',
 						code: 'https://github.com/Kirk-Terekhin/PageOnlineStore/',
 						view: 'https://kirk-terekhin.github.io/PageOnlineStore/',
-            desc: 'Верстка страницы интернет магазина для практики.',
-            tech: ['HTML5', 'CSS3', 'jQuery'],
-						html: `
-                           <p>Верстка страницы интернет магазина для практики.</p>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                                <li>jQuery</li>
-                            </ul>
-                            `
+						desc: 'Верстка страницы интернет магазина для практики.<br />Апрель 2016',
+						tech: ['HTML5', 'CSS3', 'jQuery']
 					},
-          {
-            number: 11,
-            src: publicPath + './images/Karacenter.jpg',
+					{
+						src: publicPath + './images/Karacenter.jpg',
 						alt: 'KaraCenter',
 						name: 'KaraCenter',
 						code: 'https://github.com/Kirk-Terekhin/KaraCenter',
 						view: 'https://karacenterkg.000webhostapp.com/',
-            desc: 'Создание Landing Page на CMS Joomla.',
-            tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive', 'CMS Joomla'],
-						html: `
-                           <p>Создание Landing Page на CMS Joomla.</p>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                                <li>Bootstrap</li>
-                                <li>Adaptive</li>
-                                <li>Responsive</li>
-                                <li>CMS Joomla</li>
-                            </ul>
-                            `
+						desc: 'Создание Landing Page на CMS Joomla.<br />2015',
+						tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive', 'CMS Joomla']
 					},
-          {
-            number: 12,
-            src: publicPath + './images/Connect.jpg',
+					{
+						src: publicPath + './images/Connect.jpg',
 						alt: 'Connect.kg',
 						name: 'Connect.kg',
 						code: 'https://github.com/Kirk-Terekhin/Connect',
 						view: 'https://connect-kg.000webhostapp.com/',
-            desc: 'Колл центр Connect. CMS Joomla.',
-            tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive', 'CMS Joomla'],
-						html: `
-                           <p>Колл центр Connect. CMS Joomla.</p>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                                <li>Bootstrap</li>
-                                <li>Adaptive</li>
-                                <li>Responsive</li>
-                                <li>CMS Joomla</li>
-                            </ul>
-                            `
+						desc: 'Колл центр Connect. CMS Joomla.<br />2016',
+						tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive', 'CMS Joomla']
 					},
-          {
-            number: 13,
-            src: publicPath + './images/Family.jpg',
+					{
+						src: publicPath + './images/Family.jpg',
 						alt: 'Family.kg',
 						name: 'Family.kg',
 						code: 'https://github.com/Kirk-Terekhin/Family',
 						view: 'https://familykg.000webhostapp.com/',
-            desc: 'Дом отдыха "Семья". CMS Joomla.',
-            tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive', 'CMS Joomla'],
-						html: `
-                           <p>Дом отдыха "Семья". CMS Joomla.</p>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                                <li>Bootstrap</li>
-                                <li>Adaptive</li>
-                                <li>Responsive</li>
-                                <li>CMS Joomla</li>
-                            </ul>
-                            `
+						desc: 'Дом отдыха "Семья". CMS Joomla.<br />2015',
+						tech: ['HTML5', 'CSS3', 'Bootstrap', 'Adaptive', 'Responsive', 'CMS Joomla']
 					},
-          {
-            number: 14,
-            src: publicPath + './images/Toe-to-Toes.jpg',
+					{
+						src: publicPath + './images/Toe-to-Toes.jpg',
 						alt: 'Toe to Toes',
 						name: 'Toe to Toes',
 						code: 'https://github.com/Kirk-Terekhin/Landings/tree/gh-pages/logstyle/',
 						view: 'https://kirk-terekhin.github.io/Landings/logstyle',
-            desc: '...',
-            tech: ['HTML5', 'CSS3'],
-						html: `
-                           <p>...</p>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                            </ul>
-                            `
+						desc: '...<br /> 2014',
+						tech: ['HTML5', 'CSS3']
 					},
-          {
-            number: 15,
-            src: publicPath + './images/DrumTab.jpg',
+					{
+						src: publicPath + './images/DrumTab.jpg',
 						alt: 'Drum Tab',
 						name: 'Drum Tab',
 						code: 'https://github.com/Kirk-Terekhin/Drum-Tab',
 						view: 'https://ragnarokdrum.stackblitz.io/list',
-            desc: 'Первое приложение на Angular (4v).Без стилей,только функционал.',
-            tech: ['HTML5', 'CSS3', 'Angular'],
-						html: `
-                           <p>Первое приложение на Angular (4v).Без стилей,только функционал.</p>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                                <li>Angular</li>
-                            </ul>
-                            `
+						desc: 'Первое приложение на Angular (4v).Без стилей,только функционал.<br />Май 2017',
+						tech: ['HTML5', 'CSS3', 'Angular']
 					},
-          {
-            number: 16,
-            src: publicPath + './images/YgarockAPP.jpg',
+					{
+						src: publicPath + './images/YgarockAPP.jpg',
 						alt: 'YgarockAPP',
 						name: 'YgarockAPP',
 						code: 'https://github.com/Deathmetalldrummer/YgarockAPP/',
 						view: 'https://deathmetalldrummer.github.io/YgarockAPP/source/build/',
-            desc: '...',
-            tech: ['HTML5', 'CSS3', 'jQuery', 'Bootstrap'],
-						html: `
-                           <p>...</p>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                                <li>jQuery</li>
-                                <li>Bootstrap</li>
-                            </ul>
-                            `
+						desc: '...<br />Сентябрь 2019',
+						tech: ['HTML5', 'CSS3', 'jQuery', 'Bootstrap']
 					}
 				]
 			}
